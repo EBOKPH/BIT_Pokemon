06c015975c004fbf34391f8f172ecb247d4850a6
EBOKPH <202110230@feualabang.edu.ph>
2026-04-01 17:27:49 +0800

    meh

--- api.js
+++ api.js
@@ -3,7 +3,8 @@
 // Uses window.API_BASE_URL set from .env CLIENT_URL in deployment
 // export const API_BASE_URL = window.API_BASE_URL || "http://localhost:4000";
 
- export const API_BASE_URL = window.API_BASE_URL || "https://bit-pokemon-backend.onrender.com";
+export const API_BASE_URL =
+  window.API_BASE_URL || "https://bit-pokemon-backend.onrender.com";
 
 // ========================
 // PAGE BASE URL — Handles both local and GitHub Pages deployment
